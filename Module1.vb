a398adf232ad648841212817b88a467d013c4220
Rifandi19 <muhammadrifandi64@gmail.com>
2025-10-05 12:15:00 +0700

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+    Sub Main()
+        ' === Input Data ===
+        Console.Write("Masukkan Nama: ")
+        Dim nama As String = Console.ReadLine()
+
+        Console.Write("Masukkan Program Studi: ")
+        Dim prodi As String = Console.ReadLine()
+
+        Console.Write("Masukkan Nama Kampus: ")
+        Dim kampus As String = Console.ReadLine()
+
+        Console.Write("Masukkan Hobi: ")
+        Dim hobi As String = Console.ReadLine()
+
+        ' === Output Biodata ===
+        Console.WriteLine()
+        Console.WriteLine("Halo, nama saya adalah " & nama)
+        Console.WriteLine("Saya kuliah pada jurusan " & prodi & " di " & kampus)
+        Console.WriteLine("Saya memiliki hobi " & hobi)
+
+        ' Agar console tidak langsung tertutup
+        Console.WriteLine()
+        Console.WriteLine("Tekan SPASI untuk keluar...")
+        Console.ReadLine()
+    End Sub
+End Module
